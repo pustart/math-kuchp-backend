3aca5d47789e11b29fa71317da6ea6949f1a874f
pustart <polaris9955@gmail.com>
2023-06-09 09:49:29 +0300

    fix: update db config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: env("DATABASE_CLIENT", "postgres"),
-    connector: 'bookshelf',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: env('DATABASE_CLIENT', 'postgres'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+      },
+      options: {
+        debug: false,
+      },
     },
-    debug: false,
   },
 });
